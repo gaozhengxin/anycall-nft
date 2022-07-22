7e5bcc9b91f28e6f495147f3317b52e67021e879
gaozhengxin <zhengxingao@live.cn>
2022-07-22 12:31:17 +0800

    init commit

---
+++ config.js
@@ -0,0 +1,20 @@
+[
+    {
+        "name":"fantam animals",
+        "250":{
+            "type":"ERC721",
+            "nft":"",
+            "gateway":""
+        },
+        "1":{
+            "type":"ERC721",
+            "nft":"",
+            "gateway":""
+        },
+        "43114":{
+            "type":"ERC721",
+            "nft":"",
+            "gateway":""
+        }
+    }
+]
